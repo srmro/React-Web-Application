389287a07cb2ba8e869b486d837ef8a786c94b35
smro <833stephanie@gmail.com>
2025-03-04 03:01:20 -0700

    re-arranging the routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import RecipeData from '../components/RecipeData.jsx';
-import About from '../components/About.jsx';
-import MyCookBook from '../components/MyCookBook.jsx';
-import Instructions from '../components/Details.jsx';
+import { BrowserRouter } from 'react-router-dom'
 
-
-const router = createBrowserRouter([
-    {path:"/", element:<RecipeData/>},
-    {path:"/about", element:<About/>},
-    {path:"/MyCookBook", element:<MyCookBook/>},
-    {path:"/Instructions/:id", element:<Instructions/>}
-]);
 createRoot(document.getElementById('root')).render(
-   <>
+   <StrictMode>
        <App />
-    <RouterProvider router={router} />
+   
 
-    </>
+    </StrictMode>
 )
